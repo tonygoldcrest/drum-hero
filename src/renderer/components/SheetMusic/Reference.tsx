3021792edb690ccd4e22b1904d684089ec563f09
peancored <peancored@gmail.com>
2026-06-28 14:05:57 +0100

    Update reference

--- src/renderer/components/SheetMusic/Reference.tsx
+++ src/renderer/components/SheetMusic/Reference.tsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { cn } from '../../cn';
 import { KIT_ELEMENTS } from '../../constants';
 import { elementIcon } from '../../util';
+import { faCircle, faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import themedark from '../../theme';
 
 interface ReferenceProps {
   className?: string;
@@ -12,7 +14,7 @@ export function Reference({ className }: ReferenceProps) {
   return (
     <div
       className={cn(
-        'bg-paper-light px-2 py-1 rounded-xl border border-ink/13 text-ink shadow-paper-strong flex items-center text-lg',
+        'bg-paper-light px-2 py-1 rounded-xl border border-ink/13 text-ink shadow-paper-strong flex items-center',
         className,
       )}
     >
@@ -31,6 +33,29 @@ export function Reference({ className }: ReferenceProps) {
           </div>
         );
       })}
+      <div className="flex items-center">
+        <Divider vertical className="bg-ink/13" />
+        <div className="px-2 py-0.2 flex items-center gap-2 text-nowrap">
+          <FontAwesomeIcon
+            icon={faGreaterThan}
+            color={themedark.color.ink}
+            size="lg"
+          />
+          <div className="font-ui font-semibold">Hit hard</div>
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <Divider vertical className="bg-ink/13" />
+        <div className="px-2 py-0.2 flex items-center gap-2 text-nowrap">
+          <div className="font-display flex items-center">
+            <div className="text-xl mt-0.75">(</div>
+            <FontAwesomeIcon icon={faCircle} color={themedark.color.ink} />
+            <div className="text-xl mt-0.75">)</div>
+          </div>
+          <div className="font-ui font-semibold">Hit softly</div>
+        </div>
+      </div>
     </div>
   );
 }
